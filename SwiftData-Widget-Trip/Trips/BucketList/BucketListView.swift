c76f48e6360500c29a52344aa28a6fa252bc87b0
Thiha Ye Yint Aung <thihayeyintaung110919@gmail.com>
2025-11-17 09:34:01 -0600

    group files

---
+++ SwiftData-Widget-Trip/Trips/BucketList/BucketListView.swift
@@ -0,0 +1,19 @@
+//
+//  BucketListView.swift
+//  SwiftData-Widget-Trip
+//
+//  Created by Thiha Ye Yint Aung on 11/17/25.
+//
+
+import SwiftUI
+
+struct BucketListView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BucketListView()
+}
